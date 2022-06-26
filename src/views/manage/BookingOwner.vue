e50ad8670bf23e2f8f93e96f2e413a95af807c3f
thangdp-ekoios <thang.duong@ekoios.vn>
2022-06-26 15:02:04 +0700

    fix bug

---
+++ src/views/manage/BookingOwner.vue
@@ -0,0 +1,122 @@
+<template lang="html">
+  <div>
+    <v-header
+      :has-button="false"
+      title-icon="booking"
+      :button-text="$t('button.add')"
+      title-text="Quản lý đơn hàng của các sân"
+    />
+
+    <main class="content-main-container">
+      <div class="box-shadow bg-white rounded-lg p-[1em]">
+        <div class="flex box-shadow-1 rounded-md w-fit px-[16px] py-[0.5rem]">
+          <v-icon icon-class="cart" class="text-[80px] mr-[8px]" />
+          <div>
+            <p class="text-lg font-bold text-[#3a3a3a] mb-[0.5em]">
+              Tổng số đơn hàng
+            </p>
+            <p class="text-center text-[#00b5ad] text-[30px]">86</p>
+          </div>
+        </div>
+
+        <h2 class="my-[1em] text-lg font-500">Thông tin chi tiết</h2>
+
+        <v-table
+          :table-data="results"
+          :columns="cols"
+          :limit="limit"
+          :page="page"
+          :total="total"
+        >
+          <template slot="status">
+            <div
+              class="text-center bg-[#00b5ad] text-[white] rounded-lg py-[0.5rem] w-[85%] mx-auto"
+            >
+              Đã thanh toán
+            </div>
+          </template>
+          <template slot="action">
+            <div class="text-center">
+              <el-button type="primary" icon="el-icon-check">Pay</el-button>
+            </div>
+          </template>
+        </v-table>
+      </div>
+    </main>
+  </div>
+</template>
+<script>
+import { getOrderOwner } from '../../apis/order'
+export default {
+  data() {
+    return {
+      loading: false,
+      isOpen: false,
+      total: 1,
+      page: 1,
+      limit: 20,
+      gas: 10,
+      results: [
+        {
+          station: 'Sân Nam Từ Liêm',
+          name: 'Thằng DP',
+          phone: '12345678',
+          time: '13:30 23/12/2021',
+          created_at: '23/12/2021'
+        }
+      ],
+
+      cols: [
+        {
+          prop: 'station',
+          label: 'Sân',
+          minWidth: '20'
+        },
+        {
+          prop: 'name',
+          label: 'Người đặt vé',
+          minWidth: '15'
+        },
+        {
+          prop: 'phone',
+          label: 'Số điện thoại',
+          minWidth: '15'
+        },
+        {
+          prop: 'time',
+          label: 'Thời gian sử dụng',
+          minWidth: '15'
+        },
+        {
+          prop: 'money',
+          label: 'Số tiền',
+          minWidth: '10'
+        },
+
+        {
+          prop: 'created_at',
+          label: 'Ngày đặt',
+          minWidth: '10'
+        }
+      ]
+    }
+  },
+  async created() {
+    const data = await (await getOrderOwner()).data
+    console.log(data)
+    this.results = data.map((res) => {
+      return {
+        station: `${res.place.name}-${res.place.address}`,
+        name: '',
+        phone: res.phoneNumber,
+        money: res.money +'VND',
+        time: res.timeBlocks[0].timeStart,
+        created_at: res.createAt
+      }
+    })
+
+    console.log(this.results)
+  }
+}
+</script>
+<style lang=""></style>
