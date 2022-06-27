56b632e4df02cc367266d208b9c9c46db822f86f
thangdp-ekoios <thang.duong@ekoios.vn>
2022-06-27 18:40:16 +0700

    fix bug

--- src/views/manage/Dashboard.vue
+++ src/views/manage/Dashboard.vue
@@ -46,6 +46,29 @@
         </div>
       </el-col>
 
+       <el-col :xs="12" :sm="12" :md="8" class="mb-[1em]">
+        <div class="flex box-shadow-1 rounded-md w-fit px-[16px] py-[0.5rem] w-full">
+          <v-icon icon-class="debt" class="text-[80px] mr-[8px]" />
+          <div class="overflow-hidden flex-1 text-center">
+            <p class="text-lg font-bold text-[#3a3a3a] mb-[0.5em] overflow-hidden overflow-ellipsis whitespace-nowrap">
+              Số tài khoản khách hàng
+            </p>
+            <p class="text-center text-[#00b5ad] text-[24px]">{{ gasFee }}</p>
+          </div>
+        </div>
+      </el-col>
+        <el-col :xs="12" :sm="12" :md="8" class="mb-[1em]">
+        <div class="flex box-shadow-1 rounded-md w-fit px-[16px] py-[0.5rem] w-full">
+          <v-icon icon-class="debt" class="text-[80px] mr-[8px]" />
+          <div class="overflow-hidden flex-1 text-center">
+            <p class="text-lg font-bold text-[#3a3a3a] mb-[0.5em] overflow-hidden overflow-ellipsis whitespace-nowrap">
+              Số tài khoản khách chủ sân
+            </p>
+            <p class="text-center text-[#00b5ad] text-[24px]">{{ gasFee }}</p>
+          </div>
+        </div>
+      </el-col>
+
       <el-col :xs="12" :sm="12" :md="8" class="mb-[1em]">
         <div class="flex box-shadow-1 rounded-md w-fit px-[16px] py-[0.5rem] w-full">
           <v-icon icon-class="money" class="text-[80px] mr-[8px]" />
